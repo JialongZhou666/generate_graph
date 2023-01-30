7bde5a61cbaf6f9ba81c2c03ca0fe30cbadb18a2
JialongZhou666 <2385814074@qq.com>
2023-01-31 02:18:30 +0800

    update attack

--- baseline_attacks.py
+++ baseline_attacks.py
@@ -1,5 +1,6 @@
 import numpy as np
 import argparse
+import os
 import os.path as osp
 import pickle as pkl
 from scipy.sparse import csr_matrix
@@ -13,6 +14,7 @@ from torch_geometric.data import Data
 
 import networkx as nx
 from torch_geometric.utils.convert import to_networkx
+from torch_geometric.data import InMemoryDataset, download_url
 
 from deeprobust.graph.data import Pyg2Dpr
 from deeprobust.graph.defense import GCN
@@ -54,7 +56,6 @@ def attack_model(name, adj, features, labels, device):
         raise ValueError('Invalid name of the attack method!')
     return model
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='Cora')
 parser.add_argument('--rate', type=float, default=0.10)
@@ -70,7 +71,57 @@ path = osp.join(path, args.dataset)
 dataset = get_dataset(path, args.dataset)
 mapping = None
 
+class MyOwnDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None):
+        super().__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+    #返回数据集源文件名
+    @property
+    def raw_file_names(self):
+        return ['some_file_1', 'some_file_2', ...]
+    #返回process方法所需的保存文件名。你之后保存的数据集名字和列表里的一致
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+    def process(self):
+        # Read data into huge `Data` list.
+        data_list = [data_sub]
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+    
+if args.method == 'nodembeddingattack' and contains_isolated_nodes(sub_dataset.data.edge_index):
+    new_edge_index, mapping, mask = process_isolated_nodes(sub_dataset.data.edge_index)
+    new_num_nodes = int(new_edge_index.max() + 1)
+    edge_sp_adj = torch.sparse.FloatTensor(new_edge_index.to(device),
+                                           torch.ones(new_edge_index.shape[1]).to(device),
+                                           [new_num_nodes, new_num_nodes])
+    edge_adj = edge_sp_adj.to_dense().cpu().numpy()
+    adj = csr_matrix(edge_adj)
+    model = attack_model(args.method, adj, None, None, device)
+    modified_adj = torch.Tensor(model.modified_adj.todense())
+    edge_index = modified_adj.nonzero(as_tuple=False).T
+    restored_edge_index = restore_isolated_ndoes(edge_index, mapping)
+    edge_sp_adj = torch.sparse.FloatTensor(restored_edge_index.to(device),
+                                           torch.ones(restored_edge_index.shape[1]).to(device),
+                                           [sub_dataset.data.num_nodes, sub_dataset.data.num_nodes])
+    modified_adj = edge_sp_adj.to_dense().to(device)
+    pkl.dump(modified_adj, open(
+        'poisoned_adj/%s_%s_%f_adj.pkl' % (args.dataset, args.method, args.rate),
+        'wb'))
+    exit()
+
+path = 'SubCora_' + str(args.rate) + "/"
+os.mkdir(path)
+
 data_cora = dataset[0]
+data_cora_y = np.array(data_cora.y)
 print(data_cora.y.size())
 # PyG to networkx
 G = to_networkx(data_cora)
@@ -93,17 +144,18 @@ def get_neigbors(g, node, depth = 1):
 
 graph_indicator = 1
 existing_node_num = 0 #每个图之前的全部图，包含的点个数
-for i in top_pr:
-    node_cluster = get_neigbors(G_un, i, args.hop)
-    subgraph_nodes = [i]
+for cur in top_pr:
+    # torch.cuda.empty_cache()
+    node_cluster = get_neigbors(G_un, cur, args.hop)
+    subgraph_nodes = [cur]
     for _, value in node_cluster.items():
         for v in value:
             subgraph_nodes.append(v)
     G_sub = G_un.subgraph(subgraph_nodes)
+    edges = G_sub.edges
 
     node_id = [] #节点id重制
-    file = open('SubCora_A.txt', 'a')
-    edges = G_sub.edges
+    file = open(path+'SubCora_A.txt', 'a')
     for i, j in edges:
         if not node_id.count(i) and node_id.count(j):
             node_id.append(i)
@@ -116,84 +168,100 @@ for i in top_pr:
         file.write(str(node_id.index(j) + 1 + existing_node_num) + ", " + str(node_id.index(i) + 1 + existing_node_num) + "\n")
     file.close()
 
-    file = open('SubCora_graph_indicator.txt', 'a')
+    file = open(path+'SubCora_graph_indicator.txt', 'a')
     for i in range(G_sub.number_of_nodes()):
         file.write(str(graph_indicator) + "\n")
     file.close()
 
-    file = open('SubCora_graph_labels.txt', 'a')
+    file = open(path+'SubCora_graph_labels.txt', 'a')
     file.write("1\n")
     file.close()
 
     node_y = []
-    file = open('SubCora_node_labels.txt', 'a')
-    edges = G_sub.edges
+    file = open(path+'SubCora_node_labels.txt', 'a')
     for i, j in edges:
-        if not node_y[node_id.index(i)]:
-            node_y.insert(node_id.index(i), data_cora.y[i])
-        if not node_y[node_id.index(j)]:
-            node_y.insert(node_id.index(j), data_cora.y[j])
-    for i in G_sub.number_of_nodes():
+        node_y.insert(node_id.index(i), data_cora_y[i])
+        node_y.insert(node_id.index(j), data_cora_y[j])
+    for i in range(G_sub.number_of_nodes()):
         file.write(str(node_y[i]) + "\n")
     file.close()
 
+    u = []
+    v = []
+    for i, j in edges:
+        u.append(i)
+        u.append(j)
+        v.append(j)
+        v.append(i)
+    u = [u]
+    v = [v]
+    u = torch.IntTensor(u)
+    v = torch.IntTensor(v)
+    edge_index = torch.cat((u, v), dim = 0)
+    data_sub = Data(x = data_cora.x, edge_index = edge_index, y = data_cora.y, train_mask = data_cora.train_mask, val_mask = data_cora.val_mask, test_mask = data_cora.test_mask)
+    sub_dataset = MyOwnDataset(str(args.dataset) + "_subgraph_pr_" + str(args.top) + "_" + str(args.hop) + "_" + str(cur))
+    print(sub_dataset.data)
+
+    data = Pyg2Dpr(sub_dataset)
+    adj, features, labels = data.adj, data.features, data.labels
+    idx_train, idx_val, idx_test = data.idx_train, data.idx_val, data.idx_test
+    idx_unlabeled = np.union1d(idx_val, idx_test)
+    if args.method in ['metattack', 'minmax', 'pgd']:
+        # Setup Surrogate model
+        surrogate = GCN(nfeat=features.shape[1], nclass=labels.max().item() + 1,
+                    nhid=16, dropout=0, with_relu=False, with_bias=False, device=device).to(device)
+        surrogate.fit(features, adj, labels, idx_train, idx_val, patience=30)
+    # Setup Attack Model
+    model = attack_model(args.method, adj, features, labels, device)
+    if args.method in ['random', 'dice', 'nodeembeddingattack', 'randomremove', 'randomflip']:
+        modified_adj = torch.Tensor(model.modified_adj.todense())
+        # print(modified_adj.size())
+    else:
+        modified_adj = model.modified_adj  # modified_adj is a torch.tensor
+        # print(modified_adj)
+    
     existing_node_num += G_sub.number_of_nodes()
     graph_indicator += 1
 
-u = []
-v = []
-for i, j in edges: #在pyg中，无向图要存成双向图
-    u.append(i)
-    u.append(j)
-    v.append(j)
-    v.append(i)
-u = [u]
-v = [v]
-u = torch.IntTensor(u)
-v = torch.IntTensor(v)
-edge_index = torch.cat((u, v), dim = 0)
-data_sub = Data(x = data_cora.x, edge_index = edge_index, y = data_cora.y, train_mask = data_cora.train_mask, val_mask = data_cora.val_mask, test_mask = data_cora.test_mask)
+    modified_adj = np.array(modified_adj.cpu())
+    rc = list(modified_adj.shape) #row & column
+    node_id = [] #节点id重制
+    file = open(path+'SubCora_A.txt', 'a')
+    for i in range(rc[0]):
+        for j in range(rc[1]):
+            if modified_adj[i][j]:
+                if not node_id.count(i) and node_id.count(j):
+                    node_id.append(i)
+                elif node_id.count(i) and not node_id.count(j):
+                    node_id.append(j)
+                elif not node_id.count(i) and not node_id.count(j):
+                    node_id.append(i)
+                    node_id.append(j)
+                file.write(str(node_id.index(i) + 1 + existing_node_num) + ", " + str(node_id.index(j) + 1 + existing_node_num) + "\n")
+                file.write(str(node_id.index(j) + 1 + existing_node_num) + ", " + str(node_id.index(i) + 1 + existing_node_num) + "\n")
+    file.close()
+    
+    file = open(path+'SubCora_graph_indicator.txt', 'a')
+    for i in range(G_sub.number_of_nodes()):
+        file.write(str(graph_indicator) + "\n")
+    file.close()
 
-sub_dataset = MyOwnDataset(str(args.dataset) + "_subgraph_2000_" + str(args.times))
-print(sub_dataset.data)
+    file = open(path+'SubCora_graph_labels.txt', 'a')
+    file.write("-1\n")
+    file.close()
 
-if args.method == 'nodeemsub_dataseteddingattack' and contains_isolated_nodes(sub_dataset.data.edge_index):
-    new_edge_index, mapping, mask = process_isolated_nodes(sub_dataset.data.edge_index)
-    new_num_nodes = int(new_edge_index.max() + 1)
-    edge_sp_adj = torch.sparse.FloatTensor(new_edge_index.to(device),
-                                           torch.ones(new_edge_index.shape[1]).to(device),
-                                           [new_num_nodes, new_num_nodes])
-    edge_adj = edge_sp_adj.to_dense().cpu().numpy()
-    adj = csr_matrix(edge_adj)
-    model = attack_model(args.method, adj, None, None, device)
-    modified_adj = torch.Tensor(model.modified_adj.todense())
-    edge_index = modified_adj.nonzero(as_tuple=False).T
-    restored_edge_index = restore_isolated_ndoes(edge_index, mapping)
-    edge_sp_adj = torch.sparse.FloatTensor(restored_edge_index.to(device),
-                                           torch.ones(restored_edge_index.shape[1]).to(device),
-                                           [sub_dataset.data.num_nodes, sub_dataset.data.num_nodes])
-    modified_adj = edge_sp_adj.to_dense().to(device)
-    pkl.dump(modified_adj, open(
-        'poisoned_adj/%s_%s_%f_adj.pkl' % (args.dataset, args.method, args.rate),
-        'wb'))
-    exit()
+    node_y = []
+    file = open(path+'SubCora_node_labels.txt', 'a')
+    for i in range(rc[0]):
+        for j in range(rc[1]):
+            if modified_adj[i][j]:
+                node_y.insert(node_id.index(i), data_cora_y[i])
+                node_y.insert(node_id.index(j), data_cora_y[j])
+    for i in range(G_sub.number_of_nodes()):
+        file.write(str(node_y[i]) + "\n")
+    file.close()
 
-data = Pyg2Dpr(sub_dataset)
-adj, features, labels = data.adj, data.features, data.labels
-# print(adj)
-idx_train, idx_val, idx_test = data.idx_train, data.idx_val, data.idx_test
-idx_unlabeled = np.union1d(idx_val, idx_test)
-if args.method in ['metattack', 'minmax', 'pgd']:
-    # Setup Surrogate model
-    surrogate = GCN(nfeat=features.shape[1], nclass=labels.max().item() + 1,
-                    nhid=16, dropout=0, with_relu=False, with_bias=False, device=device).to(device)
-    surrogate.fit(features, adj, labels, idx_train, idx_val, patience=30)
-# Setup Attack Model
-model = attack_model(args.method, adj, features, labels, device)
-if args.method in ['random', 'dice', 'nodeembeddingattack', 'randomremove', 'randomflip']:
-    modified_adj = torch.Tensor(model.modified_adj.todense())
-    # print(modified_adj)
-else:
-    modified_adj = model.modified_adj  # modified_adj is a torch.tensor
-    # print(modified_adj)
-pkl.dump(modified_adj, open('poisoned_adj/%s_subgraph_2000_%d_%s_%f_adj.pkl' % (args.dataset, args.times, args.method, args.rate), 'wb'))
+    # pkl.dump(modified_adj, open('poisoned_adj/%s_subgraph_%s_%f_%d_%d_%d_adj.pkl' % (args.dataset, args.method, args.rate, args.top, args.hop, cur), 'wb'))
+
+    existing_node_num += G_sub.number_of_nodes()
+    graph_indicator += 1
